aa9072f805a011992edf1e8d77c633db784bb770
AntonioJimenez01 <89932599+AntonioJimenez01@users.noreply.github.com>
2024-05-09 17:10:51 -0600

    Api a Mysql

--- Program.cs
+++ Program.cs
@@ -2,53 +2,50 @@ using KomalliAPI.Contexts;
 using KomalliAPI.Clientes.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Agregar connectionString
-
 var connectionString = builder.Configuration.GetConnectionString("TestConnection");
 
 // Agregar context
+builder.Services.AddDbContext<KomalliIdentityContext>(options =>
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 23)))
+);
 
-builder.Services.AddDbContext<KomalliIdentityContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddDbContext<KomalliContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<KomalliContext>(options =>
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 23)))
+);
 
 // Agregar servicios Identity
-
 builder.Services.AddAuthorization();
 
 // Activar API Identity
-
 builder.Services.AddIdentityApiEndpoints<Cliente>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<KomalliIdentityContext>();
 
+// Agregar servicios de controladores
+builder.Services.AddControllers();
 
-// Inicializar Roles
-
-using (var scope = builder.Services.BuildServiceProvider().CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-    await SeedRoles(roleManager);
-}
-
-    // Add services to the container.
-
-    builder.Services.AddControllers();
-
-// Configurar swagger
-
+// Configurar Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Construir app
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Obtener el servicio de DbContext para aplicar migraciones
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<KomalliContext>();
 
+    // Aplicar migraciones pendientes automáticamente al iniciar la aplicación
+    dbContext.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -57,13 +54,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.UseAuthentication();
-
 app.MapControllers();
-
 app.Run();
 
 
